2d2c4a635c2e33409c2d65463f906e1722d054ba
CamilaBornis <camila.bornis@gmail.com>
2019-08-21 10:07:13 -0300

    Inicio y carousel

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<?php
+  include_once("php/base-de-datos.php");
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -43,7 +46,7 @@
      </div>
 
      <!--  CAROUSEL -->
-       <select class="container fluid bg-dark">
+       <main class="container fluid bg-dark position">
          <div class="bd-example">
            <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
              <ol class="carousel-indicators">
@@ -72,11 +75,14 @@
          </a>
        </div>
      </div>
-   </select>
+   </main>
      <!-- Fin del carousel -->
      <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+    <!-- ---------CAJA2---------- -->
+
 
 </body>
 </html>
